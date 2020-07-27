201455f5d84b9ac6774ff1e3b283592f4899dcd5
Dio <178069857@qq.com>
2020-07-27 13:52:53 +0800

    Update 1_base.sh

--- 1_base.sh
+++ 1_base.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # 桌面端/服务端基础配置
 
+apt-get update
+
+apt-get install -y curl
+apt-get install -y vim git 
+
 apt-get install -y python3-pip
 apt-get install -y nginx
 apt-get install -y mongodb
