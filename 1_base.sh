0282f7cea3d5b3c6701354fab7a2264e4248ca93
Dio <178069857@qq.com>
2022-11-07 00:28:39 +0800

    添加了源的文件更新

--- 1_base.sh
+++ 1_base.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 # 桌面端/服务端基础配置
+
+cp /etc/apt/sources.list /etc/apt/sources.list.back
+
+echo "deb http://mirrors.aliyun.com/debian/ buster main non-free contrib
+deb http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
+deb http://mirrors.aliyun.com/debian/ buster-backports main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ buster main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ buster-backports main non-free contrib
+deb http://mirrors.aliyun.com/debian-security/ buster/updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian-security/ buster/updates main non-free contrib
+" > /etc/apt/sources.list
 
 apt-get update
 
