f3bc6aca42b71b36b3fcbcf86342a537dc99a46e
Dio <178069857@qq.com>
2022-11-08 22:42:26 +0800

    Update 1_base.sh

--- 1_base.sh
+++ 1_base.sh
@@ -14,7 +14,7 @@ deb-src http://mirrors.aliyun.com/debian-security/ buster/updates main non-free 
 " > /etc/apt/sources.list
 
 apt-get update
-apt-get install -y curl vim git python3-pip apt-transport-https ca-certificates gnupg lsb-release
+apt-get install -y curl vim git python3-pip apt-transport-https ca-certificates gnupg lsb-release net-tools
 
 echo "alias ll='ls -l'" >> /etc/profile
 echo "alias tf='tail -f'" >> /etc/profile
